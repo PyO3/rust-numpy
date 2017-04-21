226148d586871fc6224d2abe3166265e26a2ab9c
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-21 21:43:30 +0900

    PyArrayNeighborhoodIterObject

--- numpy-sys/src/pyarrayiter.rs
+++ numpy-sys/src/pyarrayiter.rs
@@ -42,3 +42,29 @@ pub struct PyArrayMultiIterObject {
     pub dimensions: [npy_intp; 32usize],
     pub iters: [*mut PyArrayIterObject; 32usize],
 }
+
+#[repr(C)]
+#[derive(Clone, Copy)]
+pub struct PyArrayNeighborhoodIterObject {
+    pub ob_base: PyObject,
+    pub nd_m1: ::std::os::raw::c_int,
+    pub index: npy_intp,
+    pub size: npy_intp,
+    pub coordinates: [npy_intp; 32usize],
+    pub dims_m1: [npy_intp; 32usize],
+    pub strides: [npy_intp; 32usize],
+    pub backstrides: [npy_intp; 32usize],
+    pub factors: [npy_intp; 32usize],
+    pub ao: *mut PyArrayObject,
+    pub dataptr: *mut ::std::os::raw::c_char,
+    pub contiguous: npy_bool,
+    pub bounds: [[npy_intp; 2usize]; 32usize],
+    pub limits: [[npy_intp; 2usize]; 32usize],
+    pub limits_sizes: [npy_intp; 32usize],
+    pub translate: npy_iter_get_dataptr_t,
+    pub nd: npy_intp,
+    pub dimensions: [npy_intp; 32usize],
+    pub _internal_iter: *mut PyArrayIterObject,
+    pub constant: *mut ::std::os::raw::c_char,
+    pub mode: ::std::os::raw::c_int,
+}
