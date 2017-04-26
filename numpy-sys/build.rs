2a8398c3e323dd4b5bba8d927f34f15abd9531cd
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-27 01:45:27 +0900

    Build script for numpy source

--- numpy-sys/build.rs
+++ numpy-sys/build.rs
@@ -1,30 +1,27 @@
 
-extern crate cpython;
+extern crate glob;
 
-use std::env::var;
 use std::path::PathBuf;
 use std::process::Command;
-use cpython::*;
+use glob::glob;
 
-fn get_module_path(py: Python) -> PyResult<String> {
-    let ma = py.import("numpy.core.multiarray")?;
-    let path: String = ma.get(py, "__file__")?.extract(py)?;
-    Ok(path)
-}
 
 fn main() {
-    let output = PathBuf::from(var("OUT_DIR").unwrap().replace(r"\", "/"));
-    let gil = Python::acquire_gil();
-    let path = PathBuf::from(get_module_path(gil.python()).unwrap());
-    let dir = path.parent().unwrap();
-    let name = path.file_name().unwrap().to_str().unwrap();
+    let source = PathBuf::from("numpy");
+    let libpath = PathBuf::from("numpy/build/lib.linux-x86_64-3.6/numpy/core");
+
+    Command::new("python").args(&["setup.py", "build"]).current_dir(&source).status().expect("Failed to build numpy");
+
+    let lib: PathBuf = glob(libpath.join("multiarray.*.so").to_str().unwrap()).unwrap().last().unwrap().unwrap();
+    let name = lib.file_name().unwrap();
 
     Command::new("ln")
-        .arg("-s")
-        .arg(path.to_str().unwrap())
-        .arg(output.join("libmultiarray.so").to_str().unwrap())
-        .output()
-        .expect("Faild to create symbolic link");
-    println!("cargo:rustc-link-search={}", output.display());
+        .arg("-sf")
+        .arg(name)
+        .arg("libmultiarray.so")
+        .current_dir(&libpath)
+        .status()
+        .expect("Failed to create symlink");
+    println!("cargo:rustc-link-search={}", libpath.display());
     println!("cargo:rustc-link-lib=dylib=multiarray");
 }
