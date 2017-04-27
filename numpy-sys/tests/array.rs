4a7604c28c576c4deaabb29f58020d52a32f7d9f
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-27 17:51:14 +0900

    Test for PyArray_Arange (fail)

--- numpy-sys/tests/array.rs
+++ numpy-sys/tests/array.rs
@@ -2,6 +2,7 @@
 extern crate python3_sys as pyffi;
 extern crate numpy_sys;
 
+use pyffi::*;
 use numpy_sys::*;
 use std::os::raw::c_void;
 
@@ -10,12 +11,19 @@ unsafe fn get_api(offset: isize) -> *const *const c_void {
     api.offset(offset)
 }
 
-/// check link succeeds
 #[test]
-fn test_link() {
+fn test_no_pyobject() {
     let res = unsafe {
         let fptr = get_api(0) as (*const extern "C" fn() -> u32);
         (*fptr)()
     };
     println!("ndarray C version = {}", res);
+}
+
+#[test]
+fn test_return_pyobject() {
+    let _res = unsafe {
+        let fptr = get_api(186) as (*const extern "C" fn(f64, f64, f64, i32) -> *mut PyObject);
+        (*fptr)(0.0, 1.0, 0.1, 12)
+    };
 }
