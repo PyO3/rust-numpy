5ec6c3c7a2ab6e10469cfbbcb7750a236d155670
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-27 19:13:49 +0900

    Add link to umath

--- numpy-sys/build.rs
+++ numpy-sys/build.rs
@@ -7,22 +7,33 @@ use std::process::Command;
 use glob::glob;
 
 
+fn search_lib(libpath: &PathBuf, regex: &str) -> PathBuf {
+    glob(libpath.join(regex).to_str().unwrap()).unwrap().last().unwrap().unwrap()
+}
+
 fn main() {
     let source = PathBuf::from("numpy");
     let libpath = canonicalize(PathBuf::from("numpy/build/lib.linux-x86_64-3.6/numpy/core")).unwrap();
 
     Command::new("python").args(&["setup.py", "build"]).current_dir(&source).status().expect("Failed to build numpy");
-
-    let lib: PathBuf = glob(libpath.join("multiarray.*.so").to_str().unwrap()).unwrap().last().unwrap().unwrap();
-    let name = lib.file_name().unwrap();
+    let multiarray = search_lib(&libpath, "multiarray.*.so");
+    let umath = search_lib(&libpath, "umath.*.so");
 
     Command::new("ln")
         .arg("-sf")
-        .arg(name)
+        .arg(multiarray.file_name().unwrap())
         .arg("libmultiarray.so")
+        .current_dir(&libpath)
+        .status()
+        .expect("Failed to create symlink");
+    Command::new("ln")
+        .arg("-sf")
+        .arg(umath.file_name().unwrap())
+        .arg("libumath.so")
         .current_dir(&libpath)
         .status()
         .expect("Failed to create symlink");
     println!("cargo:rustc-link-search={}", libpath.display());
     println!("cargo:rustc-link-lib=dylib=multiarray");
+    println!("cargo:rustc-link-lib=dylib=umath");
 }
