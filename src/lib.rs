31bdfadc7a4f518a4bd9ca7320b867626e424155
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-04-29 16:46:59 +0900

    Add pointer access

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,16 @@
 extern crate python3_sys as py_ffi;
 extern crate numpy_sys as np_ffi;
 extern crate cpython;
+
+use cpython::PyObject;
+
+pub struct PyArray(PyObject);
+
+impl PyArray {
+    pub fn as_ptr(&self) -> *mut np_ffi::PyArrayObject {
+        self.0.as_ptr() as *mut np_ffi::PyArrayObject
+    }
+    pub fn steal_ptr(self) -> *mut np_ffi::PyArrayObject {
+        self.0.steal_ptr() as *mut np_ffi::PyArrayObject
+    }
+}
