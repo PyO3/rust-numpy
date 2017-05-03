968043667bcdf5972ec452a5c46b6e2d4939d0f9
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-05-03 16:55:15 +0900

    Test strides

--- tests/array.rs
+++ tests/array.rs
@@ -5,10 +5,23 @@ extern crate numpy;
 use numpy::*;
 
 #[test]
-fn array_new() {
+fn array_shapes() {
     let py = cpython::Python::acquire_gil();
-    let _arr = PyArray::zeros(py.python(),
-                              &[4, 4],
-                              NPY_TYPES::NPY_DOUBLE,
-                              NPY_ORDER::NPY_CORDER);
+    let n = 3;
+    let m = 5;
+    let arr = PyArray::zeros(py.python(),
+                             &[n, m],
+                             NPY_TYPES::NPY_DOUBLE,
+                             NPY_ORDER::NPY_CORDER);
+    assert!(arr.ndim() == 2);
+    assert!(arr.dims() == [n, m]);
+    assert!(arr.strides() == [m as isize * 8, 8]);
+
+    let arr = PyArray::zeros(py.python(),
+                             &[n, m],
+                             NPY_TYPES::NPY_DOUBLE,
+                             NPY_ORDER::NPY_FORTRANORDER);
+    assert!(arr.ndim() == 2);
+    assert!(arr.dims() == [n, m]);
+    assert!(arr.strides() == [8, n as isize * 8]);
 }
