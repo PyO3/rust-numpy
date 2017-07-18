9262d4323bde5fe858cd4c246bf500e759e7a04b
Toshiki Teramura <toshiki.teramura@gmail.com>
2017-07-18 17:28:48 +0900

    Update CI of example

--- example/setup.py
+++ example/setup.py
@@ -1,10 +1,11 @@
 
 from setuptools import setup
-from setuptools_rust import RustExtension
+from setuptools_rust import RustExtension, Binding
 
 setup(name='rust_ext',
       version='1.0',
       rust_extensions=[
-          RustExtension('rust_ext._rust_ext', 'extensions/Cargo.toml')],
+          RustExtension('rust_ext._rust_ext', 'extensions/Cargo.toml',
+                        binding=Binding.RustCPython)],
       packages=['rust_ext'],
       zip_safe=False)
