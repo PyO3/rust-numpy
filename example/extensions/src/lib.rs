1958ae1ba8a0e4ba4c86421cca504f6febbef73c
Toshiki Teramura <toshiki.teramura@gmail.com>
2018-05-04 17:18:54 +0900

    Update sample

--- example/extensions/src/lib.rs
+++ example/extensions/src/lib.rs
@@ -1,48 +1,41 @@
+#![feature(proc_macro, proc_macro_path_invoc, specialization)]
 
-#[macro_use]
-extern crate pyo3;
-extern crate numpy;
 extern crate ndarray;
+extern crate numpy;
+extern crate pyo3;
 
-use numpy::*;
 use ndarray::*;
-use pyo3::{PyResult, Python, PyObject};
+use numpy::*;
+use pyo3::{py, PyModule, PyObject, PyResult, Python};
 
-/* Pure rust-ndarray functions */
+#[py::modinit(rust_ext)]
+fn init_module(py: Python, m: &PyModule) -> PyResult<()> {
+    // immutable example
+    fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
+        a * &x + &y
+    }
 
-// immutable example
-fn axpy(a: f64, x: ArrayViewD<f64>, y: ArrayViewD<f64>) -> ArrayD<f64> {
-    a * &x + &y
-}
+    // mutable example (no return)
+    fn mult(a: f64, mut x: ArrayViewMutD<f64>) {
+        x *= a;
+    }
 
-// mutable example (no return)
-fn mult(a: f64, mut x: ArrayViewMutD<f64>) {
-    x *= a;
-}
+    // wrapper of `axpy`
+    #[pyfn(m, "axpy")]
+    fn axpy_py(py: Python, a: f64, x: PyArray, y: PyArray) -> PyResult<PyArray> {
+        let np = PyArrayModule::import(py)?;
+        let x = x.as_array().into_pyresult(py, "x must be f64 array")?;
+        let y = y.as_array().into_pyresult(py, "y must be f64 array")?;
+        Ok(axpy(a, x, y).into_pyarray(py, &np))
+    }
 
-/* rust-pyo3 wrappers (to be exposed) */
+    // wrapper of `mult`
+    #[pyfn(m, "mult")]
+    fn mult_py(py: Python, a: f64, x: PyArray) -> PyResult<PyObject> {
+        let x = x.as_array_mut().into_pyresult(py, "x must be f64 array")?;
+        mult(a, x);
+        Ok(py.None()) // Python function must returns
+    }
 
-// wrapper of `axpy`
-fn axpy_py(py: Python, a: f64, x: PyArray, y: PyArray) -> PyResult<PyArray> {
-    let np = PyArrayModule::import(py)?;
-    let x = x.as_array().into_pyresult(py, "x must be f64 array")?;
-    let y = y.as_array().into_pyresult(py, "y must be f64 array")?;
-    Ok(axpy(a, x, y).into_pyarray(py, &np))
-}
-
-// wrapper of `mult`
-fn mult_py(py: Python, a: f64, x: PyArray) -> PyResult<PyObject> {
-    let x = x.as_array_mut().into_pyresult(py, "x must be f64 array")?;
-    mult(a, x);
-    Ok(py.None()) // Python function must returns
-}
-
-/* Define module "_rust_ext" */
-py_module_initializer!(_rust_ext, init_rust_ext, PyInit__rust_ext, |py, m| {
-    m.add(py, "__doc__", "Rust extension for NumPy")?;
-    m.add(py,
-             "axpy",
-             py_fn!(py, axpy_py(a: f64, x: PyArray, y: PyArray)))?;
-    m.add(py, "mult", py_fn!(py, mult_py(a: f64, x: PyArray)))?;
     Ok(())
-});
+}
