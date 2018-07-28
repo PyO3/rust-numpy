96cb439b27f619e5f87e04f36b1d5aadb1651a33
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-07-28 15:05:41 +0900

    Format

--- src/error.rs
+++ src/error.rs
@@ -23,10 +23,7 @@ impl<T, E: IntoPyErr> IntoPyResult for Result<T, E> {
 /// Error for casting `PyArray` into `ArrayView` or `ArrayViewMut`
 #[derive(Debug)]
 pub enum ArrayCastError {
-    ToRust {
-        test: i32,
-        truth: i32,
-    },
+    ToRust { test: i32, truth: i32 },
     FromVec,
 }
 
@@ -39,12 +36,10 @@ impl ArrayCastError {
 impl fmt::Display for ArrayCastError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            ArrayCastError::ToRust { test, truth } =>  {
+            ArrayCastError::ToRust { test, truth } => {
                 write!(f, "Cast failed: from={}, to={}", test, truth)
             }
-            ArrayCastError::FromVec => {
-                write!(f, "Cast failed: FromVec (maybe invalid dimension)")
-            }
+            ArrayCastError::FromVec => write!(f, "Cast failed: FromVec (maybe invalid dimension)"),
         }
     }
 }
@@ -52,7 +47,7 @@ impl fmt::Display for ArrayCastError {
 impl error::Error for ArrayCastError {
     fn description(&self) -> &str {
         match self {
-            ArrayCastError::ToRust {..} => "ArrayCast failed(IntoArray)",
+            ArrayCastError::ToRust { .. } => "ArrayCast failed(IntoArray)",
             ArrayCastError::FromVec => "ArrayCast failed(FromVec)",
         }
     }
@@ -61,7 +56,7 @@ impl error::Error for ArrayCastError {
 impl IntoPyErr for ArrayCastError {
     fn into_pyerr(self, msg: &str) -> PyErr {
         let msg = match self {
-            ArrayCastError::ToRust {..} => {
+            ArrayCastError::ToRust { .. } => {
                 format!("rust_numpy::ArrayCastError::IntoArray: {}", msg)
             }
             ArrayCastError::FromVec => format!("rust_numpy::ArrayCastError::FromVec: {}", msg),
