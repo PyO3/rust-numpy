e4cda62b2587a4c9f11a46bbf81418eca5b6f39a
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-08-18 18:46:39 +0900

    Refactor

--- src/types.rs
+++ src/types.rs
@@ -35,30 +35,36 @@ impl NpyDataType {
             x if x == NPY_TYPES::NPY_BYTE as i32 => NpyDataType::Int8,
             x if x == NPY_TYPES::NPY_SHORT as i32 => NpyDataType::Int16,
             x if x == NPY_TYPES::NPY_INT as i32 => NpyDataType::Int32,
-            x if x == NPY_TYPES::NPY_LONG as i32 => {
-                if cfg!(any(windows, target_pointer_width = "32")) {
-                    NpyDataType::Int32
-                } else {
-                    NpyDataType::Int64
-                }
-            }
+            x if x == NPY_TYPES::NPY_LONG as i32 => NpyDataType::from_clong(false),
             x if x == NPY_TYPES::NPY_LONGLONG as i32 => NpyDataType::Int64,
             x if x == NPY_TYPES::NPY_UBYTE as i32 => NpyDataType::Uint8,
             x if x == NPY_TYPES::NPY_USHORT as i32 => NpyDataType::Uint16,
             x if x == NPY_TYPES::NPY_UINT as i32 => NpyDataType::Uint32,
-            x if x == NPY_TYPES::NPY_ULONG as i32 => {
-                if cfg!(any(windows, target_pointer_width = "32")) {
-                    NpyDataType::Uint32
-                } else {
-                    NpyDataType::Uint64
-                }
-            }
+            x if x == NPY_TYPES::NPY_ULONG as i32 => NpyDataType::from_clong(true),
             x if x == NPY_TYPES::NPY_ULONGLONG as i32 => NpyDataType::Uint64,
             x if x == NPY_TYPES::NPY_FLOAT as i32 => NpyDataType::Float32,
             x if x == NPY_TYPES::NPY_DOUBLE as i32 => NpyDataType::Float64,
             x if x == NPY_TYPES::NPY_CFLOAT as i32 => NpyDataType::Complex32,
             x if x == NPY_TYPES::NPY_CDOUBLE as i32 => NpyDataType::Complex64,
             _ => NpyDataType::Unsupported,
+        }
+    }
+    #[inline(always)]
+    fn from_clong(is_usize: bool) -> NpyDataType {
+        if cfg!(any(target_pointer_width = "32", windows)) {
+            if is_usize {
+                NpyDataType::Uint32
+            } else {
+                NpyDataType::Int32
+            }
+        } else if cfg!(all(target_pointer_width = "64", not(windows))) {
+            if is_usize {
+                NpyDataType::Uint64
+            } else {
+                NpyDataType::Int64
+            }
+        } else {
+            NpyDataType::Unsupported
         }
     }
 }
@@ -84,7 +90,7 @@ macro_rules! impl_type_num {
             }
         }
     };
-} // impl_type_num!
+}
 
 impl_type_num!(bool, Bool, NPY_BOOL);
 impl_type_num!(i8, Int8, NPY_BYTE);
@@ -97,12 +103,12 @@ impl_type_num!(c32, Complex32, NPY_CFLOAT);
 impl_type_num!(c64, Complex64, NPY_CDOUBLE);
 
 cfg_if! {
-    if #[cfg(any(windows, target_pointer_width = "32"))] {
+    if #[cfg(any(target_pointer_width = "32", windows))] {
         impl_type_num!(i32, Int32, NPY_INT, NPY_LONG);
         impl_type_num!(u32, Uint32, NPY_UINT, NPY_ULONG);
         impl_type_num!(i64, Int64, NPY_LONGLONG);
         impl_type_num!(u64, Uint64, NPY_ULONGLONG);
-    } else {
+    } else if #[cfg(all(target_pointer_width = "64", not(windows)))] {
         impl_type_num!(i32, Int32, NPY_INT);
         impl_type_num!(u32, Uint32, NPY_UINT);
         impl_type_num!(i64, Int64, NPY_LONG, NPY_LONGLONG);
