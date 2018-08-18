977b25617ad32d35b63667e44ab62ec5a535e3d3
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-08-18 15:15:06 +0900

    Remove debug output for appveyor

--- src/array.rs
+++ src/array.rs
@@ -35,15 +35,14 @@ impl<'a, T: TypeNum> FromPyObject<'a> for &'a PyArray<T> {
     fn extract(ob: &'a PyObjectRef) -> PyResult<Self> {
         let array = unsafe {
             if npyffi::PyArray_Check(ob.as_ptr()) == 0 {
-                println!("extract: array check failed");
                 return Err(PyDowncastError.into());
             }
             &*(ob as *const PyObjectRef as *const PyArray<T>)
         };
-        array.type_check().map(|_| array).map_err(|err| {
-            println!("extract: type check failed");
-            err.into_pyerr("FromPyObject::extract typecheck failed")
-        })
+        array
+            .type_check()
+            .map(|_| array)
+            .map_err(|err| err.into_pyerr("FromPyObject::extract typecheck failed"))
     }
 }
 
