40fceeed867858c5dadd5f48ee664660e5574e45
kngwyu <yuji.kngw.80s.revive@gmail.com>
2018-08-21 15:55:15 +0900

    Fix redirection

--- ci/travis/deploy.sh
+++ ci/travis/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cargo doc --all-features --no-deps &&
-echo "<meta http-equiv=refresh content=0;url=os_balloon/index.html>" > target/doc/index.html &&
+echo "<meta http-equiv=refresh content=0;url=numpy/index.html>" > target/doc/index.html &&
 git clone https://github.com/davisp/ghp-import.git &&
 ./ghp-import/ghp_import.py -n \
                            -p \
