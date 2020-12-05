cfe630f15277949810801bf7ed65027b6e463db8
kngwyu <yuji.kngw.80s.revive@gmail.com>
2020-12-06 00:54:09 +0900

    Update numpy-linalg-example with maturin and poetry

--- examples/linalg/src/lib.rs
+++ examples/linalg/src/lib.rs
@@ -1,20 +1,17 @@
-#![deny(rust_2018_idioms)]
 use ndarray_linalg::solve::Inverse;
-use numpy::{IntoPyArray, PyArray2};
-use pyo3::exceptions::RuntimeError;
-use pyo3::prelude::{pymodule, Py, PyErr, PyModule, PyResult, Python};
-use std::fmt::Display;
-
-fn make_error<E: Display + Sized>(e: E) -> PyErr {
-    PyErr::new::<RuntimeError, _>(format!("[rust_linalg] {}", e))
-}
+use numpy::{IntoPyArray, PyArray2, PyReadonlyArray2};
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::prelude::{pymodule, PyErr, PyModule, PyResult, Python};
 
 #[pymodule]
 fn rust_linalg(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     #[pyfn(m, "inv")]
-    fn inv(py: Python<'_>, x: &PyArray2<f64>) -> PyResult<Py<PyArray2<f64>>> {
-        let x = x.as_array().inv().map_err(make_error)?;
-        Ok(x.into_pyarray(py).to_owned())
+    fn inv<'py>(py: Python<'py>, x: PyReadonlyArray2<'py, f64>) -> PyResult<&'py PyArray2<f64>> {
+        let x = x
+            .as_array()
+            .inv()
+            .map_err(|e| PyErr::new::<PyRuntimeError, _>(format!("[rust_linalg] {}", e)))?;
+        Ok(x.into_pyarray(py))
     }
     Ok(())
 }
