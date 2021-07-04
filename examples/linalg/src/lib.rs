cdb1ed9890809b2cb0815efd579cedd5c54f2393
Adam Reichold <adam.reichold@t-online.de>
2021-07-04 18:51:34 +0200

    Avoid deprecation warnings in example projects.

--- examples/linalg/src/lib.rs
+++ examples/linalg/src/lib.rs
@@ -5,7 +5,7 @@ use pyo3::prelude::{pymodule, PyErr, PyModule, PyResult, Python};
 
 #[pymodule]
 fn rust_linalg(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    #[pyfn(m, "inv")]
+    #[pyfn(m)]
     fn inv<'py>(py: Python<'py>, x: PyReadonlyArray2<'py, f64>) -> PyResult<&'py PyArray2<f64>> {
         let x = x
             .as_array()
