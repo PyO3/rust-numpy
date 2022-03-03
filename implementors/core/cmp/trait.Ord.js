4bc3af77cba9360a6c62bebb04990d11925edaaf
adamreichold <adamreichold@users.noreply.github.com>
2022-03-03 21:20:04 +0000

    Upload documentation for current main

--- implementors/core/cmp/trait.Ord.js
+++ implementors/core/cmp/trait.Ord.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["numpy"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.59.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"numpy/npyffi/types/enum.NPY_TYPES.html\" title=\"enum numpy::npyffi::types::NPY_TYPES\">NPY_TYPES</a>","synthetic":false,"types":["numpy::npyffi::types::NPY_TYPES"]}];
+implementors["numpy"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.58.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"enum\" href=\"numpy/npyffi/types/enum.NPY_TYPES.html\" title=\"enum numpy::npyffi::types::NPY_TYPES\">NPY_TYPES</a>","synthetic":false,"types":["numpy::npyffi::types::NPY_TYPES"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
