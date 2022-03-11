e4c32d450427e939770b03ee5351940f18c2bebb
Adam Reichold <adam.reichold@t-online.de>
2022-03-11 13:46:16 +0100

    Add benchmarks for creating one-dimensional arrays.

---
+++ benches/array.rs
@@ -0,0 +1,143 @@
+#![feature(test)]
+
+extern crate test;
+use test::{black_box, Bencher};
+
+use std::ops::Range;
+
+use numpy::PyArray1;
+use pyo3::{Python, ToPyObject};
+
+struct Iter(Range<usize>);
+
+impl Iterator for Iter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+}
+
+fn from_iter(bencher: &mut Bencher, size: usize) {
+    bencher.iter(|| {
+        let iter = black_box(Iter(0..size));
+
+        Python::with_gil(|py| {
+            PyArray1::from_iter(py, iter);
+        });
+    });
+}
+
+#[bench]
+fn from_iter_small(bencher: &mut Bencher) {
+    from_iter(bencher, 2_usize.pow(5));
+}
+
+#[bench]
+fn from_iter_medium(bencher: &mut Bencher) {
+    from_iter(bencher, 2_usize.pow(10));
+}
+
+#[bench]
+fn from_iter_large(bencher: &mut Bencher) {
+    from_iter(bencher, 2_usize.pow(15));
+}
+
+struct ExactIter(Range<usize>);
+
+impl Iterator for ExactIter {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next()
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.0.size_hint()
+    }
+}
+
+impl ExactSizeIterator for ExactIter {
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+}
+
+fn from_exact_iter(bencher: &mut Bencher, size: usize) {
+    bencher.iter(|| {
+        let iter = black_box(ExactIter(0..size));
+
+        Python::with_gil(|py| {
+            PyArray1::from_exact_iter(py, iter);
+        });
+    });
+}
+
+#[bench]
+fn from_exact_iter_small(bencher: &mut Bencher) {
+    from_exact_iter(bencher, 2_usize.pow(5));
+}
+
+#[bench]
+fn from_exact_iter_medium(bencher: &mut Bencher) {
+    from_exact_iter(bencher, 2_usize.pow(10));
+}
+
+#[bench]
+fn from_exact_iter_large(bencher: &mut Bencher) {
+    from_exact_iter(bencher, 2_usize.pow(15));
+}
+
+fn from_slice(bencher: &mut Bencher, size: usize) {
+    let vec = (0..size).collect::<Vec<_>>();
+
+    bencher.iter(|| {
+        let slice = black_box(&vec);
+
+        Python::with_gil(|py| {
+            PyArray1::from_slice(py, slice);
+        });
+    });
+}
+
+#[bench]
+fn from_slice_small(bencher: &mut Bencher) {
+    from_slice(bencher, 2_usize.pow(5));
+}
+
+#[bench]
+fn from_slice_medium(bencher: &mut Bencher) {
+    from_slice(bencher, 2_usize.pow(10));
+}
+
+#[bench]
+fn from_slice_large(bencher: &mut Bencher) {
+    from_slice(bencher, 2_usize.pow(15));
+}
+
+fn from_object_slice(bencher: &mut Bencher, size: usize) {
+    let vec = Python::with_gil(|py| (0..size).map(|val| val.to_object(py)).collect::<Vec<_>>());
+
+    bencher.iter(|| {
+        let slice = black_box(&vec);
+
+        Python::with_gil(|py| {
+            PyArray1::from_slice(py, slice);
+        });
+    });
+}
+
+#[bench]
+fn from_object_slice_small(bencher: &mut Bencher) {
+    from_object_slice(bencher, 2_usize.pow(5));
+}
+
+#[bench]
+fn from_object_slice_medium(bencher: &mut Bencher) {
+    from_object_slice(bencher, 2_usize.pow(10));
+}
+
+#[bench]
+fn from_object_slice_large(bencher: &mut Bencher) {
+    from_object_slice(bencher, 2_usize.pow(15));
+}
