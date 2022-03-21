5c857ce0bbbf8f8ee837b0e453f1c2b8903a7dcf
adamreichold <adamreichold@users.noreply.github.com>
2022-03-21 09:18:41 +0000

    Upload documentation for current main

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["numpy"] = {"name":"","dirs":[{"name":"npyffi","files":["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]}],"files":["array.rs","convert.rs","dtype.rs","error.rs","lib.rs","npyiter.rs","readonly.rs","slice_container.rs","sum_products.rs"]};
+sourcesIndex["numpy"] = {"name":"","dirs":[{"name":"npyffi","files":["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]}],"files":["array.rs","borrow.rs","convert.rs","dtype.rs","error.rs","lib.rs","npyiter.rs","slice_container.rs","sum_products.rs"]};
 createSourceSidebar();
