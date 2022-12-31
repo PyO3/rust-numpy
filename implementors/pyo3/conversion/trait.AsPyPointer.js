ae62ddd2e8c46a2120f69312a26b344cd9e9bc4c
adamreichold <adamreichold@users.noreply.github.com>
2022-12-31 17:20:14 +0000

    Upload documentation for current main

--- implementors/pyo3/conversion/trait.AsPyPointer.js
+++ implementors/pyo3/conversion/trait.AsPyPointer.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["numpy"] = [{"text":"impl&lt;T, D&gt; AsPyPointer for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;","synthetic":false,"types":["numpy::array::PyArray"]},{"text":"impl AsPyPointer for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>","synthetic":false,"types":["numpy::dtype::PyArrayDescr"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"numpy":[["impl&lt;T, D&gt; AsPyPointer for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;"],["impl AsPyPointer for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- source-files.js
+++ source-files.js
@@ -1,4 +1,4 @@
 var sourcesIndex = JSON.parse('{\
-"numpy":["",[["npyffi",[],["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]]],["array.rs","borrow.rs","convert.rs","datetime.rs","dtype.rs","error.rs","lib.rs","slice_container.rs","sum_products.rs"]]\
+"numpy":["",[["borrow",[],["mod.rs","shared.rs"]],["npyffi",[],["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]]],["array.rs","convert.rs","datetime.rs","dtype.rs","error.rs","lib.rs","slice_container.rs","sum_products.rs"]]\
 }');
 createSourceSidebar();
