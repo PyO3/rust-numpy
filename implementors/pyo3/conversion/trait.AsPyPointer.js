259df1648f6a353d4e33676f94e3d7dfff097195
adamreichold <adamreichold@users.noreply.github.com>
2023-03-05 18:23:11 +0000

    Upload documentation for current main

--- implementors/pyo3/conversion/trait.AsPyPointer.js
+++ implementors/pyo3/conversion/trait.AsPyPointer.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"numpy":[["impl&lt;T, D&gt; AsPyPointer for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;"],["impl AsPyPointer for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"]]
+"numpy":[["impl&lt;T, D&gt; AsPyPointer for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;"],["impl AsPyPointer for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"],["impl AsPyPointer for <a class=\"struct\" href=\"numpy/struct.PyUntypedArray.html\" title=\"struct numpy::PyUntypedArray\">PyUntypedArray</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
