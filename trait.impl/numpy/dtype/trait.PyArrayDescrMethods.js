c30f8d80910ec563542c32ec66126c12138159f7
adamreichold <adamreichold@users.noreply.github.com>
2024-03-23 07:58:49 +0000

    Upload documentation for current main

---
+++ trait.impl/numpy/dtype/trait.PyArrayDescrMethods.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"numpy":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
