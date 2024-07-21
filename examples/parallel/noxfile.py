161cb2a0af137280607faaa0ebe1328f9ac99654
Adam Reichold <adam.reichold@t-online.de>
2024-07-21 15:28:09 +0200

    Explicitly install NumPy 1.x as we are not compatible with NumPy 2.x yet.

--- examples/parallel/noxfile.py
+++ examples/parallel/noxfile.py
@@ -3,6 +3,6 @@ import nox
 
 @nox.session
 def tests(session):
-    session.install("pip", "numpy", "pytest")
+    session.install("pip", "numpy<2", "pytest")
     session.run("pip", "install", ".", "-v")
     session.run("pytest")
