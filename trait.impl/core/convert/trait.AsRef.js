c585248e406aed7ca62647d2b155d25e76bca85a
davidhewitt <davidhewitt@users.noreply.github.com>
2024-11-22 12:44:16 +0000

    Upload documentation for current main

--- trait.impl/core/convert/trait.AsRef.js
+++ trait.impl/core/convert/trait.AsRef.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["numpy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;PyAny&gt; for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;PyAny&gt; for <a class=\"struct\" href=\"numpy/struct.PyUntypedArray.html\" title=\"struct numpy::PyUntypedArray\">PyUntypedArray</a>"],["impl&lt;T, D&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;PyAny&gt; for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;"]]]]);
+    var implementors = Object.fromEntries([["numpy",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;PyAny&gt; for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.82.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;PyAny&gt; for <a class=\"struct\" href=\"numpy/struct.PyUntypedArray.html\" title=\"struct numpy::PyUntypedArray\">PyUntypedArray</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[873]}
+//{"start":57,"fragment_lengths":[573]}
--- trait.impl/pyo3/instance/trait.PyNativeType.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    var implementors = Object.fromEntries([["numpy",[["impl PyNativeType for <a class=\"struct\" href=\"numpy/struct.PyArrayDescr.html\" title=\"struct numpy::PyArrayDescr\">PyArrayDescr</a>"],["impl PyNativeType for <a class=\"struct\" href=\"numpy/struct.PyUntypedArray.html\" title=\"struct numpy::PyUntypedArray\">PyUntypedArray</a>"],["impl&lt;T, D&gt; PyNativeType for <a class=\"struct\" href=\"numpy/array/struct.PyArray.html\" title=\"struct numpy::array::PyArray\">PyArray</a>&lt;T, D&gt;"]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":57,"fragment_lengths":[459]}
