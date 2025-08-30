816a77f80b9d092873e1c2818fafd480fb93a667
davidhewitt <davidhewitt@users.noreply.github.com>
2025-08-30 09:17:59 +0000

    Upload documentation for current main

--- src-files.js
+++ src-files.js
@@ -1,3 +1,2 @@
-var srcIndex = new Map(JSON.parse('[["numpy",["",[["borrow",[],["mod.rs","shared.rs"]],["npyffi",[],["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]]],["array.rs","array_like.rs","convert.rs","datetime.rs","dtype.rs","error.rs","lib.rs","slice_container.rs","strings.rs","sum_products.rs","untyped_array.rs"]]]]'));
-createSrcSidebar();
-//{"start":36,"fragment_lengths":[293]}
+createSrcSidebar('[["numpy",["",[["borrow",[],["mod.rs","shared.rs"]],["npyffi",[],["array.rs","flags.rs","mod.rs","objects.rs","types.rs","ufunc.rs"]]],["array.rs","array_like.rs","convert.rs","datetime.rs","dtype.rs","error.rs","lib.rs","slice_container.rs","strings.rs","sum_products.rs","untyped_array.rs"]]]]');
+//{"start":19,"fragment_lengths":[293]}
